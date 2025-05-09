cd82419d6f72a0a15110df15321129d7a1f93ce7
Luca Diegel <Luca@trenz.de>
2025-05-09 13:38:46 +0200

    create dao for medicine in factory

--- src/main/java/de/hitec/nhplus/datastorage/DaoFactory.java
+++ src/main/java/de/hitec/nhplus/datastorage/DaoFactory.java
@@ -21,4 +21,8 @@ public class DaoFactory {
     public PatientDao createPatientDAO() {
         return new PatientDao(ConnectionBuilder.getConnection());
     }
+
+    public MedicineDao createMedicineDAO() {
+        return new MedicineDao(ConnectionBuilder.getConnection());
+    }
 }
