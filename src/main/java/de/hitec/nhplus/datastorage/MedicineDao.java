b63b0a887c7424e45c2390b055acb42cdeba59fa
Luca Diegel <Luca@trenz.de>
2025-05-09 13:33:35 +0200

    created model and dao to add medicine to db

---
+++ src/main/java/de/hitec/nhplus/datastorage/MedicineDao.java
@@ -0,0 +1,123 @@
+package de.hitec.nhplus.datastorage;
+
+import de.hitec.nhplus.model.Medicine;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ * Implements the Interface <code>DaoImp</code>. Overrides methods to generate specific <code>PreparedStatements</code>,
+ * to execute the specific SQL Statements.
+ */
+public class MedicineDao extends DaoImp<Medicine> {
+
+    /**
+     * The constructor initiates an object of <code>MedicineDao</code> and passes the connection to its super class.
+     *
+     * @param connection Object of <code>Connection</code> to execute the SQL-statements.
+     */
+    public MedicineDao(Connection connection) {
+        super(connection);
+    }
+
+    @Override
+    protected PreparedStatement getCreateStatement(Medicine medicine) {
+        PreparedStatement preparedStatement = null;
+        try {
+            final String SQL = "INSERT INTO medicine (name, storage, expirationDate) " +
+                    "VALUES (?, ?, ?)";
+            preparedStatement = this.connection.prepareStatement(SQL);
+            preparedStatement.setString(1, medicine.getName());
+            preparedStatement.setString(2, medicine.getStorage());
+            preparedStatement.setString(3, medicine.getExpirationDate());
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return preparedStatement;
+    }
+
+    @Override
+    protected PreparedStatement getReadByIDStatement(long key) {
+        PreparedStatement preparedStatement = null;
+        try {
+            final String SQL = "SELECT * FROM medicine WHERE mid = ?";
+            preparedStatement = this.connection.prepareStatement(SQL);
+            preparedStatement.setLong(1, key);
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return preparedStatement;
+    }
+
+    @Override
+    protected Medicine getInstanceFromResultSet(ResultSet set) throws SQLException {
+        return new Medicine(
+                set.getInt(1),
+                set.getString(2),
+                set.getString(3),
+                set.getString(4));
+    }
+
+    @Override
+    protected PreparedStatement getReadAllStatement() {
+        PreparedStatement preparedStatement = null;
+        try {
+            final String SQL = "SELECT * FROM medicine";
+            preparedStatement = this.connection.prepareStatement(SQL);
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return preparedStatement;
+    }
+
+    @Override
+    protected ArrayList<Medicine> getListFromResultSet(ResultSet set) throws SQLException {
+        ArrayList<Medicine> list = new ArrayList<>();
+        while (set.next()) {
+            Medicine medicine = new Medicine(
+                    set.getInt(1),
+                    set.getString(2),
+                    set.getString(3),
+                    set.getString(4));
+            list.add(medicine);
+        }
+        return list;
+    }
+
+
+    @Override
+    protected PreparedStatement getUpdateStatement(Medicine medicine) {
+        PreparedStatement preparedStatement = null;
+        try {
+            final String SQL = "UPDATE medicine SET " +
+                    "name = ?, " +
+                    "storage = ?, " +
+                    "expirationDate = ? " +
+                    "WHERE mid = ?";
+            preparedStatement = this.connection.prepareStatement(SQL);
+            preparedStatement.setString(1, medicine.getName());
+            preparedStatement.setString(2, medicine.getStorage());
+            preparedStatement.setString(3, medicine.getExpirationDate());
+            preparedStatement.setLong(4, medicine.getMid());
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return preparedStatement;
+    }
+
+    @Override
+    protected PreparedStatement getDeleteStatement(long key) {
+        PreparedStatement preparedStatement = null;
+        try {
+            final String SQL = "DELETE FROM medicine WHERE mid = ?";
+            preparedStatement = this.connection.prepareStatement(SQL);
+            preparedStatement.setLong(1, key);
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return preparedStatement;
+    }
+}
---
+++ src/main/java/de/hitec/nhplus/model/Medicine.java
@@ -0,0 +1,88 @@
+package de.hitec.nhplus.model;
+
+import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ * Medicines are used by the patient to treat their symptoms.
+ */
+public class Medicine {
+    private SimpleLongProperty mid;
+    private final SimpleStringProperty name;
+    private final SimpleStringProperty storage;
+    private final SimpleStringProperty expirationDate;
+
+    /**
+     * Constructor to initiate an object of class <code>Medicine</code> with the given parameter. Use this constructor
+     * to initiate objects, which are not persisted yet, because it will not have a medicine id (mid).
+     *
+     * @param name Name of the medicine.
+     * @param storage Storage of the medicine.
+     * @param expirationDate Expiration date of the medicine.
+     */
+    public Medicine(String name, String storage, String expirationDate) {
+        this.name = new SimpleStringProperty(name);
+        this.storage = new SimpleStringProperty(storage);
+        this.expirationDate = new SimpleStringProperty(expirationDate);
+    }
+
+    /**
+     * Constructor to initiate an object of class <code>Medicine</code> with the given parameter. Use this constructor
+     * to initiate objects, which are already persisted and have a medicine id (mid).
+     *
+     * @param mid Medicine id.
+     * @param name Name of the medicine.
+     * @param storage Storage of the medicine.
+     * @param expirationDate Expiration date of the medicine.
+     */
+    public Medicine(long mid, String name, String storage, String expirationDate) {
+        this.mid = new SimpleLongProperty(mid);
+        this.name = new SimpleStringProperty(name);
+        this.storage = new SimpleStringProperty(storage);
+        this.expirationDate = new SimpleStringProperty(expirationDate);
+    }
+
+    public long getMid() {
+        return mid.get();
+    }
+
+    public SimpleLongProperty midProperty() {
+        return mid;
+    }
+
+    public void setMid(long mid) {
+        this.mid.set(mid);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public SimpleStringProperty nameProperty() {
+        return name;
+    }
+
+    public String getStorage() {
+        return storage.get();
+    }
+
+    public SimpleStringProperty storageProperty() {
+        return storage;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate.get();
+    }
+
+    public SimpleStringProperty expirationDateProperty() {
+        return expirationDate;
+    }
+
+    public String toString() {
+        return "Medicine" + "\nMID: " + this.mid +
+                "\nName: " + this.name +
+                "\nStorage: " + this.storage +
+                "\nExpirationDate: " + this.expirationDate +
+                "\n";
+    }
+}
